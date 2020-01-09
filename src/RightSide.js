e935704f36264330b4a94cd382b76f9fb72889cd
sujaymec <sujay.mec@gmail.com>
2020-01-10 02:32:12 +0530

    adding nav bar

--- src/RightSide.js
+++ src/RightSide.js
@@ -4,9 +4,17 @@ export default class RightSide extends Component{
     render(){
         return(
             <div id="right-side">
-                <div id="options-bar">Options bar</div>
-                <div id="filters-bar">Filter bar</div>
-                <div id="projects-container">The projects list</div>
+                <div id="options-bar">
+                    <ul className="gh-grey-text">
+                        <li>Overview</li>
+                        <li className="active">Repositories <span className="badge">11</span></li>
+                        <li>Stars <span className="badge">5</span></li>
+                        <li>Followers <span className="badge">2</span></li>
+                        <li>Following <span className="badge">2</span></li>
+                    </ul>
+                </div>
+                <div id="filters-bar"></div>
+                <div id="projects-container"></div>
             </div>
         )
     }
