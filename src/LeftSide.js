6cd766b093da329e78df9ae8e8c2e3a65618ec94
sujaymec <sujay.mec@gmail.com>
2020-01-10 01:45:47 +0530

    sidebar complete

--- src/LeftSide.js
+++ src/LeftSide.js
@@ -4,15 +4,21 @@ export default class LeftSide extends Component{
     render(){
         return(
             <div id="left-side">
-                <img src={this.props.userDetails.avatar_url} alt="" />
+                <img id="avatar" src={this.props.userDetails.avatar_url} alt="" />
                 <h2 id="user-name">{this.props.userDetails.name}</h2>
-                <h3 id="user-id">{this.props.userDetails.login}</h3>
-                <p id="user-bio">{this.props.userDetails.bio}</p>
-                <button id="edit-bio">Edit Bio</button>
+                <h3 id="user-id" className="gh-grey-text">{this.props.userDetails.login}</h3>
+                <p id="user-bio" className="gh-grey-text">{this.props.userDetails.bio}</p>
+                <button className="gh-btn" id="edit-bio">Edit Bio</button>
                 <hr/>
-                <p className="contains-icon people">{this.props.userDetails.login}</p>
-                <p className="contains-icon map">{this.props.userDetails.login}</p>
-                <p className="contains-icon mail">{this.props.userDetails.login}</p>
+                <p className="contains-icon gh-grey-text people">
+                <svg className="octicon octicon-organization" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fillRule="evenodd" d="M16 12.999c0 .439-.45 1-1 1H7.995c-.539 0-.994-.447-.995-.999H1c-.54 0-1-.561-1-1 0-2.634 3-4 3-4s.229-.409 0-1c-.841-.621-1.058-.59-1-3 .058-2.419 1.367-3 2.5-3s2.442.58 2.5 3c.058 2.41-.159 2.379-1 3-.229.59 0 1 0 1s1.549.711 2.42 2.088C9.196 9.369 10 8.999 10 8.999s.229-.409 0-1c-.841-.62-1.058-.59-1-3 .058-2.419 1.367-3 2.5-3s2.437.581 2.495 3c.059 2.41-.158 2.38-1 3-.229.59 0 1 0 1s3.005 1.366 3.005 4z"></path></svg>
+                    <span>{this.props.userDetails.company}</span>
+                </p>
+                <p className="contains-icon gh-grey-text map">
+                <svg className="octicon octicon-location" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true"><path fillRule="evenodd" d="M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg>
+                    {this.props.userDetails.location}
+                </p>
+                <p className="contains-icon gh-grey-text mail">{this.props.userDetails.email}</p>
             </div>
         )
     }
