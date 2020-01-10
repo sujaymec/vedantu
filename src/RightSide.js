835d451ee8290c1e54905559b3a61994fcba7ad0
sujaymec <sujay.mec@gmail.com>
2020-01-10 15:11:28 +0530

    creating list of repositories

--- src/RightSide.js
+++ src/RightSide.js
@@ -1,7 +1,27 @@
 import React,{Component} from 'react'
 
 export default class RightSide extends Component{
+
     render(){
+    let repoList = <li>Not populated yet</li>
+    let repos = this.props.repositories;
+    console.log(repos)
+    let repoArray = []
+    if(repos){
+        for (var key in repos) {
+            if (repos.hasOwnProperty(key)) {
+              console.log(repos[key].name);
+              repoArray.push(repos[key]);
+            }
+          }
+          repoList  = repoArray.map((value, index) =>
+          <li className="repo-list-item" key={index}>
+              <h3 className="repo-name"><a href={value.html_url}>{value.name}</a></h3>
+              <p className="repo-description gh-grey-text">{value.description}</p>
+          </li>
+        );
+    }
+
         return(
             <div id="right-side">
                 <div id="options-bar">
@@ -14,16 +34,20 @@ export default class RightSide extends Component{
                     </ul>
                 </div>
                 <div id="filters-bar">
-                    <input className="gh-btn" type="text"/>
-                    <select className="gh-btn" name="" id="">
-                        <option value="">All</option>
+                    <input id="repo-search" className="gh-btn" type="text"/>
+                    <select className="gh-btn sku-btn repo-filter" name="" id="">
+                        <option value="">Type</option>
                     </select>
-                    <select className="gh-btn" name="" id="">
-                        <option value="">All</option>
+                    <select className="gh-btn sku-btn repo-filter" name="" id="">
+                        <option value="">Language</option>
                     </select>
                     <button id="new-repo" className="gh-btn">New</button>
                 </div>
-                <div id="projects-container"></div>
+                <div id="projects-container">
+                    <ul>
+                        {repoList}
+                    </ul>
+                </div>
             </div>
         )
     }
